8fa183cff79f135b6f3fddcea6fb421effac8e6a
JonneSaloranta <saloranta.jonne@gmail.com>
2026-03-26 23:18:09 +0200

    fix: remove unnecessary space character

--- opencrm/models.py
+++ opencrm/models.py
@@ -21,7 +21,7 @@ class Contact(models.Model):
     )
 
     def __str__(self):
-        return f"{self.company} - {self.firstname} {self.lastname} "
+        return f"{self.company} - {self.firstname} {self.lastname}"
 
 
 class Note(models.Model):
