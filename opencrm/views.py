1862af2d04d16c59ec1b6c56eaf748269221515a
JonneSaloranta <saloranta.jonne@gmail.com>
2026-03-26 22:39:16 +0200

    feat: add companies_view

--- opencrm/views.py
+++ opencrm/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+from .models import Company
+
 
 def index(request):
     context = {}
     return render(request, "opencrm/index.html", context)
+
+
+def companies_view(request):
+    companies = Company.objects.all().order_by("-updated_at")
+    context = {
+        "companies": companies,
+    }
+    return render(request, "opencrm/all_companies.html", context)
