af7d6abe1c02c15d5023fcf180d01b926185987b
JonneSaloranta <saloranta.jonne@gmail.com>
2026-03-26 21:54:42 +0200

    feat: add __str__ to Company model

--- opencrm/models.py
+++ opencrm/models.py
@@ -7,6 +7,9 @@ class Company(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Contact(models.Model):
     firstname = models.CharField()
