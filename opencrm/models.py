734cf4f53a5189d60ac75efd34d443c1fb901168
JonneSaloranta <saloranta.jonne@gmail.com>
2026-03-26 21:44:44 +0200

    feat: add Contact model and verbose_name translation

--- opencrm/models.py
+++ opencrm/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
 class Company(models.Model):
     name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+class Contact(models.Model):
+    firstname = models.CharField()
+    lastname = models.CharField(blank=True)
+    email = models.EmailField(blank=True)
+    phonenumber = models.CharField(blank=True)
+    company = models.ForeignKey(
+        Company, verbose_name=_("Company"), on_delete=models.CASCADE, blank=True
+    )
+    note = models.TextField(blank=True)
