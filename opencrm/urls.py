16b9701bad7492bf0258fc2f198c4f8d9b3cc4df
JonneSaloranta <saloranta.jonne@gmail.com>
2026-03-26 22:38:56 +0200

    feat: add app_name to urls

--- opencrm/urls.py
+++ opencrm/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "opencrm"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("companies", views.companies_view, name="companies"),
