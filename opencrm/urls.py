002b1b6749950f8f9d58bbe12782fe9154c64d11
JonneSaloranta <saloranta.jonne@gmail.com>
2026-03-28 13:32:27 +0200

    feat: add company details view url

--- opencrm/urls.py
+++ opencrm/urls.py
@@ -7,4 +7,5 @@ app_name = "opencrm"
 urlpatterns = [
     path("", views.index, name="index"),
     path("companies", views.companies_view, name="companies"),
+    path("company/<int:id>/", views.company_view, name="company_details"),
 ]
