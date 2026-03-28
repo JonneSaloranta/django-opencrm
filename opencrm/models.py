4b4c7962431096e278c05211cb5a035e7a2687ab
JonneSaloranta <saloranta.jonne@gmail.com>
2026-03-28 13:27:33 +0200

    feat: add get_absolute_url to Company model

--- opencrm/models.py
+++ opencrm/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
@@ -9,6 +10,9 @@ class Company(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("opencrm:company_details", kwargs={"id": self.id})
 
 
 class Tag(models.Model):
