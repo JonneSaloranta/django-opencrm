089a99035cb918b79d23146da62da733c2f41d16
JonneSaloranta <saloranta.jonne@gmail.com>
2026-03-29 14:31:29 +0300

    feat: add __str__ for Task model

--- opencrm/models.py
+++ opencrm/models.py
@@ -76,3 +76,7 @@ class Task(models.Model):
     is_done = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.text}"
+
