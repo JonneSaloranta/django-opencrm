dcb276e3ebc3651f3acf86ad4b8f426aae3a98e7
JonneSaloranta <saloranta.jonne@gmail.com>
2026-03-29 14:37:51 +0300

    feat: add contacts url

--- opencrm/urls.py
+++ opencrm/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("companies", views.companies_view, name="companies"),
     path("company/<int:id>/", views.company_view, name="company_details"),
+    path("contacts", views.contacts_view, name="contacts"),
     path("contact/<int:id>/", views.contact_details, name="contact_details"),
     path("api/tags/", views.all_tags_api, name="all_tags_api"),
     path("api/search-contacts/", views.contact_search_api, name="contact_search_api"),
