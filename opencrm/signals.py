71ccc8834c730c57ddd30199559668ca72f9fb2a
JonneSaloranta <saloranta.jonne@gmail.com>
2026-04-01 17:45:16 +0300

    feat: update contacts last_contacted field pre_save if task is marked as done

--- opencrm/signals.py
+++ opencrm/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.utils import timezone
 
@@ -12,9 +12,12 @@ def update_last_contacted_from_note(sender, instance, **kwargs):
     contact.save(update_fields=["last_contacted"])
 
 
-@receiver(post_save, sender=Task)
+@receiver(pre_save, sender=Task)
 def update_last_contacted_from_task(sender, instance, **kwargs):
-    if instance.is_done:
+    if not instance.pk:
+        return
+    old_task = Task.objects.get(pk=instance.pk)
+    if not old_task.is_done and instance.is_done:
         contact = instance.contact
         contact.last_contacted = timezone.now()
         contact.save(update_fields=["last_contacted"])
